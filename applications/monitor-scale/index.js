93c1e14645661c90c70304fb2b24fd49606200b2
jeenhyung <jeenhyung@hanmail.net>
2018-11-05 15:46:35 +0900

    modify monitor-scale index.js

--- applications/monitor-scale/index.js
+++ applications/monitor-scale/index.js
@@ -103,8 +103,8 @@ app.post('/loadtest/consecutive', function (req, res) {
 
 app.get('/up/:podId', function (req, res) {
   console.log('Server UP: %s', req.params.podId);
-//  etcd.setSync("pod-list/" + req.params.podId, req.params.podId);
-  res.send(etcd);
+  etcd.setSync("pod-list/" + req.params.podId, req.params.podId);
+  res.send('up done');
 });
 
 app.get('/down/:podId', function (req, res) {
